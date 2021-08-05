2ef7cc28b364fd1b039f3b31dc7e2c003f7507e4
Irina4567 <81246608+Irina4567@users.noreply.github.com>
2021-08-05 17:29:32 +0300

    Installed moduls

---
+++ create_form.py
@@ -0,0 +1,48 @@
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfform
+from reportlab.lib.colors import magenta, pink, blue, green
+import pdfrw
+from reportlab.pdfgen import canvas
+
+
+
+
+def create_simple_form():
+    c = canvas.Canvas('simple_form.pdf')
+
+    c.setFont("Courier", 20)
+    c.drawCentredString(300, 700, 'Employment Form')
+    c.setFont("Courier", 14)
+    form = c.acroForm
+
+    c.drawString(10, 650, 'First Name:')
+    #form.textfield(name='fname', tooltip='First Name',
+                  # x=110, y=635,
+                   #width=300)
+    c.drawString(10, 600, 'Last Name:')
+    form.textfield(name='lname', tooltip='Last Name',
+                   x=110, y=585,
+                   width=300)
+
+    c.drawString(10, 550, 'Address:')
+    form.textfield(name='address', tooltip='Address',
+                   x=110, y=535,
+                   width=400)
+
+    c.drawString(10, 500, 'City:')
+    form.textfield(name='city', tooltip='City',
+                   x=110, y=485)
+
+    c.drawString(250, 500, 'State:')
+    form.textfield(name='state', tooltip='State',
+                   x=350, y=485)
+
+    c.drawString(10, 450, 'Zip Code:')
+    form.textfield(name='zip_code', tooltip='Zip Code',
+                   x=110, y=435)
+
+    c.save()
+
+
+if __name__ == '__main__':
+    create_simple_form()
